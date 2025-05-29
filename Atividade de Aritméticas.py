10b0d8af5d7dac87157648f4f36acc0ed21db7b2
Murilo <murilocorrea006@gmail.com>
2025-05-29 20:26:17 -0300

    Atividade de Aritméticas.py

---
+++ Atividade de Aritméticas.py
@@ -0,0 +1,16 @@
+primeiro = int(input("Digite o primero número: "))
+
+segundo = int(input("Digite o segundo número: "))
+
+soma = primeiro + segundo
+
+subtração = primeiro - segundo
+
+multiplicação = primeiro * segundo
+
+divisão = primeiro // segundo
+
+print(soma)
+print(subtração)
+print(multiplicação)
+print(divisão)
