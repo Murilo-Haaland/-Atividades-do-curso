57af10e731ae1521fe33167757f79db7916036bc
Murilo <murilocorrea006@gmail.com>
2025-06-06 16:39:53 -0300

    Attv3.py

---
+++ Attv3.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número: "))
+contador=0
+lista=[1,2,3,4,5,6,7,8,9,10]
+
+for number in range(10):
+    contador+=1
+    mult=contador*numero
+    print(mult)
+         
